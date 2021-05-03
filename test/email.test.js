30ce16dee9803aa4b7ddf0f0fedd4861746a2022
Asunnya <ribeiroisadora.id.uff.br>
2021-05-03 12:20:02 -0300

    testing of all cases of valid email

--- test/email.test.js
+++ test/email.test.js
@@ -1,19 +1,12 @@
 const validatorEmail = require('../src/validEmail.js');
 
 const emailExample = 's@gmail.com';
-
 const emailExample1 = 'sonic@g.br';
-
 const emailExample2 = 'sônic@gmail.com';
-
 const emailExample3 = 'sonic@gmail%.com';
-
 const emailExample4 = 'sonic@@gmail.com';
-
 const emailExample5 = 'sonic@gmailcom';
-
 const emailExample6 = 'sonic@gmail..com';
-
 const emailExample7 = 'sonicgmail..com';
 
 test('Casos de email inválido: Usuário menor que 1 caractere', () => {
@@ -46,16 +39,6 @@ test('Casos de email inválido: com mais de um ponto no domínio', () => {
 test('Casos de email inválido: menos de um @ no email', () => {
   expect(validatorEmail(emailExample7)).toBe(false);
 });
-
-/*Para o email, deve ter as seguintes regras de negócio:
-● O tamanho do usuário deve ser maior que 1 caractere.
-● O tamanho do domínio deve ser maior que 3 caracteres.
-● O usuário e domínio devem ser separados pelo símbolo @.
-● Tanto o usuário, quanto o domínio não devem possuir acentuações, espaços em
-branco ou caracteres especiais, como: #, $, %, etc.
-● O domínio deve conter pelo menos um ponto. Exemplos: domain.com, domain.net,
-domain.com.br, domain.org, etc.
-*/
 
 const exampleEmail = 'us@dom.com';
 const exampleEmail2 = 'us@d.net';
